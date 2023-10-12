778db6d767a93ef2b9e0c212369d7b607a95fcef
gabrielewanzeler <gabrielewanzeler000@gmail.com>
2023-10-12 11:34:09 -0300

    feat: add filter to open units

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,29 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 
+import { Location } from 'src/app/types/location.interface';
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
-  results = [];
+
+  //@Output() submitEvent = new EventEmitter();
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
-  constructor (private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
+  constructor (private formBuilder: FormBuilder,
+    private unitService: GetUnitsService) {}
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.unitService.getAllUnits().subscribe(data=>console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
     })
+
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data.locations;
+      this.filteredResults = data.locations;
+    });
   }
 
   onSubmit(): void{
     console.log(this.formGroup.value)
+    if(!this.formGroup.value.showClosed) {
+      this.filteredResults = this.results.filter(location => location.opened ==true)
+    } else {
+      this.filteredResults = this.results;
+    }
   }
 
   onClean(): void {
